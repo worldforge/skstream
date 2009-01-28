6fb165272c74bffa416b726cff50fbecb8949857
Al Riddoch <alriddoch@googlemail.com>
2009-01-28 16:09:52 +0000

    2009-01-28  Al Riddoch  <alriddoch@zepler.org>
    	* test/basicskstreamtest.h: Fix an issue with an ambiguous constructor
    	  call thrown up by recent compilers. Strip out the RCS logging
    	  tag.

--- test/basicskstreamtest.h
+++ test/basicskstreamtest.h
@@ -19,123 +19,8 @@
 //  the Worldforge Web Site at http://www.wordforge.org.
 //
 //  Author: Dan Tomalesky
+//  Author: Alistair Riddoch
 //  Created: 2002-02-19 by Dan Tomalesky
-//
-// $Log$
-// Revision 1.9  2007-09-22 14:58:30  alriddoch
-// 2007-09-22  Al Riddoch  <alriddoch@zepler.org>
-//
-// 	* test/basicskstreamtest.h, test/skservertest.h,
-// 	  test/skstreamtestrunner.cpp, test/socketbuftest.h:
-// 	  Remove some tests for methods that no longer exist in the given
-// 	  classes.
-//
-// Revision 1.8  2005/01/14 17:36:36  alriddoch
-// 2004-01-14  Al Riddoch  <alriddoch@zepler.org>
-//
-// 	* test/basicskstreamtest.h: Socket buffers are now automatically
-// 	  deleted by the socket streams which own them.
-//
-// Revision 1.7  2003/05/06 21:53:11  alriddoch
-//  2003-05-06 Al Riddoch <alriddoch@zepler.org>
-//     - skstream/skstream.h, skstream/skstream.cpp, skstream_unix.h:
-//       Re-work basic_socket_stream so it can have either stream or datagram
-//       buffers.
-//     - ping/ping.cpp, ping/ping.h, test/basicskstreamtest.h,
-//       test/childskstreamtest.h, test/skservertest.h: Get the tests and examples
-//       building again.
-//
-// Revision 1.6  2003/05/04 21:30:16  alriddoch
-//  2003-05-04 Al Riddoch <alriddoch@zepler.org>
-//     - Sort out nasty hard tabs from address checks.
-//     - Start work on updating the checks.
-//
-// Revision 1.5  2002/06/12 20:48:40  xmp
-// The usual std namespace stuff.  IE this was made on gcc-2.95, and gcc-3.1 is stricter.
-//
-// Revision 1.4  2002/03/01 14:00:09  grimicus
-// 03/01/2002 Dan Tomalesky <grim@xynesis.com>
-//     * Changed can_accept's timeval to be all 0, so that it is just a quick
-//       poll and returns immediately (Thanks to alriddoch for the suggestion)
-//
-//     * Modified the ugly switch block I was using to print out error message
-//       names to use strerror (Thanks to James for that one)
-//
-// Revision 1.3  2002/02/26 20:33:55  grimicus
-// 02/26/2002 Dan Tomalesky <grim@xynesis.com>
-//     * Added test cases for skserver and friends
-//
-//     * Adding .cvsignore files so that it doesn't mess with non-cvs files anymore
-//
-//     * In skserver.cpp and skstream.cpp, in the close() methods, I commented out
-//       the return; in the error part of the shutdown() call because it is
-//       possible that a socket can exist without it actually having been used,
-//       so this could error out on those cases and the socket is never actually
-//       closed.
-//
-//     * In skserver.h, added can_accept() to tcp_socket_server so that it can be
-//       checked to see if the server socket has any connections available, so that
-//       accept() can then be called. (if it returns false, if accept is called,
-//       it will block until a connection is made)
-//
-//     * Removed the #include <iostream> from skserver.h and skstream.h as they are
-//       not actually needed for any of the code. (else it comes in from some other
-//       include, I'm not positive)
-//
-//     * Made some formatting changes in skserver.h along the same lines as I have
-//       been doing throughout the code.
-//
-//     * Added testClose() to basicskstreamtest.
-//
-//     * Changed the socket created in basicskstreamtest from SOCK_STREAM to
-//       SOCK_DGRAM though it doesn't make any difference what so ever in the
-//       testing.
-//
-//     * Added the skservertests into the test runner.
-//
-// Revision 1.2  2002/02/24 03:15:41  grimicus
-// 02/23/2002 Dan Tomalesky <grim@xynesis.com>
-//
-//     * Added in CVS logging variable so that changes show up in modified files
-//       This will help keep changes made by worldforge in each file that is
-//       changed as required by the GPL.
-//
-//     * Changed some parameter variables to have better meaning.
-//       (ad -> address, etc.)
-//
-//     * Added some code into tcp_sk_stream::open so that it calls setLastError()
-//       when the connection fails.
-//
-//     * Added some comments into skstream.h to better describe SOCKET_TYPE as
-//       there can be some confusion between what it is actually for
-//       (pointer/file descriptor/windows cludge of the socket) and the various
-//       types of sockets (tcp, udp, raw, etc)
-//
-//     * Changed some more formatting for readability.
-//
-//     * Uncommented some commented out code in skstream.h so that the sync()
-//       method returns 0 on the else, rather than not returning anything.
-//
-//     * Added some code into setBroadcast() so that setLastError() is called
-//       if it fails to perform the getsocketopt().
-//
-//     * Modified the test/Makefile.am to remove the header files from the SOURCES
-//       as the .h files do not seem to affect the build.
-//
-//     * Updated all the current test so that they use a socket instead of the
-//       absolutely wrong stuff I was doing before.
-//
-//     * Added tests for tcp, udp, and raw skstreams child classes.
-//
-// Revision 1.1  2002/02/21 05:11:15  grimicus
-// 2002-02-20 Dan Tomalesky <grim@xynesis.com>
-//     * Added a new test case header for basic_socket_streams
-//
-//     * Made a mod to basic_socket_stream::setOutpeer(sockaddr_in&)
-//       seemed to me it had an erroneous ! in the return
-//
-//     * Oh, and I reformatted skstream.h so its not so damn hard to look at
-//
 //
 
 #ifndef BASICSKSTREAMTEST_H
@@ -201,8 +86,10 @@ class basicskstreamtest : public CppUnit::TestCase
             }
             
             //test with crumby settings...should this fail?
-            skb = new stream_socketbuf(INVALID_SOCKET, 0, 0);
-            sks = new basic_socket_stream(*skb, (unsigned)0);
+            skb = new stream_socketbuf(INVALID_SOCKET,
+                                       (std::streamsize)0,
+                                       (std::streamsize)0);
+            sks = new basic_socket_stream(*skb, 0);
             CPPUNIT_ASSERT(sks);
             delete sks;
         }
